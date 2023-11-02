c19de4699b5134b1af4526227c4c9825692246ec
Daniel Coloma <danielcoloma@gmail.com>
2023-11-02 16:55:09 +0100

    Update app.js
    Signed-off-by: Daniel Coloma <danielcoloma@gmail.com>

--- app.js
+++ app.js
@@ -1,13 +1,42 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const port = process.env.PORT ?? 3000;
 
-app.use(express.static('public'))
+let broadcaster;
+const port = 4000;
 
-app.get('*', (req, res) => {
-    res.redirect('/');
-})
+const http = require("http");
+const server = http.createServer(app);
 
-app.listen(port, () => {
-    console.log(`App listening on port ${port}`);
-})
+//const io = require("socket.io")(server);
+//const io = require('socket.io')(server, { origins: '*:*'});
+const io = require('socket.io')(server, {
+  cors: {
+    origin: '*',
+  }
+});
+//app.use(express.static(__dirname + "/public"));
+app.use(express.static(__dirname));
+
+io.sockets.on("error", e => console.log(e));
+io.sockets.on("connection", socket => {
+  socket.on("broadcaster", () => {
+    broadcaster = socket.id;
+    socket.broadcast.emit("broadcaster");
+  });
+  socket.on("watcher", () => {
+    socket.to(broadcaster).emit("watcher", socket.id);
+  });
+  socket.on("offer", (id, message) => {
+    socket.to(id).emit("offer", socket.id, message);
+  });
+  socket.on("answer", (id, message) => {
+    socket.to(id).emit("answer", socket.id, message);
+  });
+  socket.on("candidate", (id, message) => {
+    socket.to(id).emit("candidate", socket.id, message);
+  });
+  socket.on("disconnect", () => {
+    socket.to(broadcaster).emit("disconnectPeer", socket.id);
+  });
+});
+server.listen(port, () => console.log(`Server is running on port ${port}`));
